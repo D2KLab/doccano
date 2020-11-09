d11adf82d423747fc4c0eda429bbbfd9d620c8ce
links-ai <links-ai@linksfoundation.com>
2020-11-09 18:19:30 +0100

    network does not have to be cancelled

--- restart_and_update.sh
+++ restart_and_update.sh
@@ -3,7 +3,8 @@
 docker-compose down --remove-orphans
 docker container rm -f $(docker ps --filter name=doccano -a -q)
 docker image rm -f $(docker image ls --format="{{.Repository}} {{.ID}}" | grep "^doccano" | cut -d' ' -f2)
-docker network rm $(docker network ls --format="{{.Name}} {{.ID}}" | grep "^doccano" | cut -d' ' -f2)
+# uncomment if you want to delete also networks
+#docker network rm $(docker network ls --format="{{.Name}} {{.ID}}" | grep "^doccano" | cut -d' ' -f2)
 docker volume rm doccano_static_volume
 docker volume rm doccano_www
 docker-compose -f docker-compose.prod.yml build
